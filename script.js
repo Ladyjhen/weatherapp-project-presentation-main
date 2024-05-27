02d88d3d34ca4af725a6546e04160f7c16cb826e
jsmolos <molosj@yahoo.com>
2024-05-27 19:02:25 +0800

    index

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+// Function to open the navigation menu
 function openMenu() {
     var nav = document.getElementById("main-nav");
     nav.classList.add("open");
 }
-  
+
+// Function to close the navigation menu
 function closeMenu() {
     var nav = document.getElementById("main-nav");
     nav.classList.remove("open");
 }
-  
+
+// Event listeners for menu toggle
 document.getElementById("nav-toggle").addEventListener("mouseenter", function() {
     openMenu();
 });
-  
+
 document.getElementById("main-nav").addEventListener("mouseleave", function() {
     closeMenu();
 });
-  
+
 document.addEventListener("click", function(event) {
     var nav = document.getElementById("main-nav");
     if (!nav.contains(event.target)) {
-      closeMenu();
+        closeMenu();
     }
 });
 
-// Function to check if an element is in viewport
+// Function to check if an element is in the viewport
 function isInViewport(element) {
     var bounding = element.getBoundingClientRect();
     return (
@@ -32,24 +35,22 @@ function isInViewport(element) {
     );
 }
 
-// Function to add effect to Filmotika heading when in viewport
-function addEffectToFilmotika() {
-    var filmotikaHeading = document.querySelector('#section2 h2');
-    if (isInViewport(filmotikaHeading)) {
-        filmotikaHeading.classList.add('filmotika-effect');
+// Function to add effect to WeatherApp heading when in the viewport
+function addEffectToWeatherApp() {
+    var WeatherAppHeading = document.querySelector('#section2 h2');
+    if (isInViewport(WeatherAppHeading)) {
+        WeatherAppHeading.classList.add('WeatherApp-effect');
     } else {
-        filmotikaHeading.classList.remove('filmotika-effect');
+        WeatherAppHeading.classList.remove('WeatherApp-effect');
     }
 }
 
 // Event listener for scroll event
 window.addEventListener('scroll', function() {
-    addEffectToFilmotika();
+    addEffectToWeatherApp();
 });
 
 // Call the function on page load
 window.addEventListener('load', function() {
-    addEffectToFilmotika();
+    addEffectToWeatherApp();
 });
-
-  
